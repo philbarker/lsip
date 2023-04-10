8ba797fe927bf2d5bfa676bb65ea6613eda18a21
scottbw <scott.bradley.wilson@gmail.com>
2023-04-10 17:54:20 +0100

    Added example of mixing template input plus open data and exporting to XCRI XML

--- main.py
+++ main.py
@@ -1,81 +1,25 @@
+import esfa
+from xcri import Xcri
+from lxml import etree
+import os
 import pandas as pd
 
 
-def keep_only(dataframe, columns):
-    dataframe.drop(dataframe.columns.difference(columns), axis=1, inplace=True)
-
-
 def main():
-    # https://www.gov.uk/government/publications/sfa-course-directory
-    esfa = pd.concat(
-        map(
-            pd.read_csv, ['data/ESFA_LiveCoursesWithRegionsAndVenuesReport-20220901.csv',
-                          'data/ESFA_LiveCoursesWithRegionsAndVenuesReport-20230103.csv',
-                          'data/ESFA_LiveCoursesWithRegionsAndVenuesReport-20230302.csv'
-                          ]
-        ),
-        ignore_index=True)
-    esfa['LEARN_AIM_REF'] = esfa['LEARN_AIM_REF'].astype('str')
-    esfa['LEARN_AIM_REF'] = esfa['LEARN_AIM_REF'].apply(lambda x: x.zfill(8))
-    fes = pd.read_csv('data/FES-fes-et-provider-enrolments-202223-q1.csv')
-    quals = pd.read_csv('data/LearningDelivery.csv', dtype={'LearnAimRef': object})
-    quals['LearnAimRef'] = quals['LearnAimRef'].astype(str)
-    subjects = pd.read_csv('data/SectorSubjectArea.csv')
-
-    levels = ['2','3','4','5']
-    barnet = 10000533
-
-    # SOC lookups
-    soc = pd.read_csv('data/barnet_main.csv')
-    cols=['Qualification', 'SOC 2020 Code']
-    keep_only(soc, cols)
-    soc = soc.drop_duplicates()
-
-    # Subject lookups
-    cols =['SectorSubjectAreaTier1Desc', 'SectorSubjectAreaTier2Desc', 'SectorSubjectAreaTier1', 'SectorSubjectAreaTier2']
-    subjects = subjects[(subjects['SectorSubjectAreaTier2_EffTo'].isnull()) | (subjects['SectorSubjectAreaTier2_EffTo'] == u'')]
-    subjects = subjects[(subjects['SectorSubjectAreaTier1_EffTo'].isnull()) | (subjects['SectorSubjectAreaTier1_EffTo'] == u'')]
-    subjects.drop(subjects.columns.difference(cols), axis=1, inplace=True)
-    subjects_l1 = subjects[['SectorSubjectAreaTier1', 'SectorSubjectAreaTier1Desc']]
-    subjects_l1 = pd.DataFrame({c: subjects_l1[c].unique() for c in subjects_l1})
-
-    # Qualifications
-    quals = quals[(quals['EffectiveTo'].isnull()) | (quals['EffectiveTo'] == u'')]
-    cols = ['LearnAimRef', 'LearnAimRefTitle', 'NotionalNVQLevelv2', 'SectorSubjectAreaTier1', 'SectorSubjectAreaTier2']
-    quals.drop(quals.columns.difference(cols), axis=1, inplace=True)
-
-    # Courses
-    cols = ['PROVIDER_UKPRN', 'LEARN_AIM_REF', 'COURSE_NAME', 'COURSE_DESCRIPTION']
-    esfa.drop(esfa.columns.difference(cols), axis=1, inplace=True)
-    esfa = esfa.drop_duplicates()
-
-    # Enrolments
-    cols = ['time_period', 'provider_ukprn', 'level', 'ssa_t1_desc', 'enrols']
-    fes = fes[(fes['level'].str.contains('|'.join(levels), regex=True))]
-    fes.drop(fes.columns.difference(cols), axis=1, inplace=True)
-    fes = fes[(fes['provider_ukprn'] == barnet)]
-    fes = fes.merge(subjects_l1, left_on='ssa_t1_desc', right_on='SectorSubjectAreaTier1Desc')
-    fes.to_csv('output/enrolments.csv')
-
-    # Filter to Barnet
-    courses = esfa[(esfa['PROVIDER_UKPRN'] == barnet)]
-
-    # Join courses and qualifications
-    courses = courses.merge(quals, left_on='LEARN_AIM_REF', right_on='LearnAimRef', how='left')
-
-    # Join subjects
-    courses = courses.merge(subjects, left_on='SectorSubjectAreaTier2', right_on='SectorSubjectAreaTier2', how='left')
-    courses.drop('LearnAimRef', axis=1, inplace=True)
-
-    # Filter out levels
-    courses = courses[(courses['NotionalNVQLevelv2'].str.contains('|'.join(levels), regex=True, na=True))]
-
-    # Join on SOC
-    courses = courses.merge(soc, left_on='LEARN_AIM_REF', right_on='Qualification', how='left')
-
-    courses.to_csv('output/courses.csv')
-
+    provider_ukprn = 10000533
+    # Get some open data
+    courses = esfa.create_open_data(provider_ukprn=provider_ukprn, output_path='output' + os.sep + 'courses.csv')
+    # Merge the minimum collection
+    data = pd.read_excel('examples' + os.sep + 'LSIP Minimal Data Collection Template Example.xlsx', sheet_name='Courses and qualifications', header=1)
+    data = data.dropna(axis=0, how='all')
+    data = data.dropna(axis=1, how='all')
+    data = pd.merge(left=courses, right=data, left_on='Qualification.identifier', right_on='Qualification Aim Reference')
+    data.drop(columns='Qualification Aim Reference', inplace=True)
+    data['Presentation.start'] = '2022/23'
+    xcri = Xcri(courses=data.to_dict('records'))
+    xml = etree.tostring(xcri.to_xml(), pretty_print=True, encoding='utf-8')
+    with open('output' + os.sep + 'example.xml', mode='wb') as file:
+        file.write(xml)
 
 if __name__ == '__main__':
     main()
-
